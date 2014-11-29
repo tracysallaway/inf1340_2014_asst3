8f9cd2a1a90ae7f8ccbb0b7138aecff8b5ba6e93
TRACY <tracysallaway@gmail.com>
2014-11-29 08:06:38 -0500

    Working with file name hard coded

--- test_mining.py
+++ test_mining.py
@@ -2,8 +2,8 @@
 
 """ Docstring """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Jodie Church, Tania Misquitta, Tracy Sallaway'
+__email__ = "jodie.church@mail.utoronto.ca, tania.misquitta@mail.utoronto.ca, tracy.armstrong@mail.utoronto.ca"
 
 __copyright__ = "2014 Susan Sim"
 __license__ = "MIT License"
@@ -15,9 +15,10 @@ from mining import *
 
 
 def test_goog():
-    read_stock_data("GOOG", "data/GOOG.json")
-    assert six_best_months() == [('2007/12', 693.76), ('2007/11', 676.55), ('2007/10', 637.38), ('2008/01', 599.42),
-                                 ('2008/05', 576.29), ('2008/06', 555.34)]
-    assert six_worst_months() == [('2004/09', 116.38), ('2004/10', 164.52), ('2004/11', 177.09), ('2004/12', 181.01),
-                                  ('2005/03', 181.18), ('2005/01', 192.96)]
+    read_stock_data(read_json_from_file(file_name))
+    assert six_best_months(six_best_months(results_list)) == [('2007-12', 693.76), ('2007-11', 676.55), ('2007-10', 637.38), ('2008-01', 599.42),
+                                 ('2008-05', 576.29), ('2008-06', 555.34)]
+
+    assert six_worst_months(six_worst_months(results_list)) == [('2004-08', 104.66), ('2004-09', 116.38), ('2004-10', 164.52), ('2004-11', 177.09), ('2004-12', 181.01),
+                                  ('2005-03', 181.18)]
 
